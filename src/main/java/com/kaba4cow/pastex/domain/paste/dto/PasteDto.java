6a479d22f90984331dcb65a665d243af239b785e
kaba4cow <kaba4cow@gmail.com>
2025-07-31 21:35:49 +0300

    Create PasteDto

---
+++ src/main/java/com/kaba4cow/pastex/domain/paste/dto/PasteDto.java
@@ -0,0 +1,19 @@
+package com.kaba4cow.pastex.domain.paste.dto;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class PasteDto {
+
+	private final UUID id;
+
+	private final LocalDateTime createdAt;
+
+}
---
+++ src/main/java/com/kaba4cow/pastex/domain/paste/dto/PasteMapper.java
@@ -0,0 +1,16 @@
+package com.kaba4cow.pastex.domain.paste.dto;
+
+import org.springframework.stereotype.Component;
+
+import com.kaba4cow.pastex.domain.paste.Paste;
+
+@Component
+public class PasteMapper {
+
+	public PasteDto mapToDto(Paste paste) {
+		return PasteDto.builder()//
+				.id(paste.getId())//
+				.build();
+	}
+
+}
