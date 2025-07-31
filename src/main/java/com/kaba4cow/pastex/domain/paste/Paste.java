4982a6d13df576e1c2d5e6d70a2c6657c37be577
kaba4cow <kaba4cow@gmail.com>
2025-07-31 22:51:52 +0300

    Revert "Exclude content from toString"
    This reverts commit 3220cfe53b29455364e989b903282b70cc61e059.

---
+++ src/main/java/com/kaba4cow/pastex/domain/paste/Paste.java
@@ -0,0 +1,40 @@
+package com.kaba4cow.pastex.domain.paste;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+@Table(name = "table_paste")
+public class Paste {
+
+	@Id
+	@Column(name = "column_id")
+	private UUID id;
+
+	@Column(name = "column_content", updatable = false)
+	private String content;
+
+	@CreationTimestamp
+	@Column(name = "column_created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+}
