50c49d339b4dd39d857199a706ec1ae9a8b64dd6
kaba4cow <kaba4cow@gmail.com>
2025-07-31 20:40:15 +0300

    Initial commit

---
+++ src/main/java/com/kaba4cow/pastex/PastexApplication.java
@@ -0,0 +1,13 @@
+package com.kaba4cow.pastex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaba4cow/pastex/PastexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaba4cow.pastex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
