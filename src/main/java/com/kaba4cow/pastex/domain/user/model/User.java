987f41895ac490e52f7e1bd05f69951ae4c07b4b
kaba4cow <kaba4cow@gmail.com>
2025-08-01 12:20:47 +0300

    Create User

---
+++ src/main/java/com/kaba4cow/pastex/domain/user/model/User.java
@@ -0,0 +1,58 @@
+package com.kaba4cow.pastex.domain.user.model;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "table_user")
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "column_id")
+	private Long id;
+
+	@Column(name = "column_email")
+	private String email;
+
+	@Column(name = "column_password_hash")
+	private String passwordHash;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String getPassword() {
+		return passwordHash;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+}
