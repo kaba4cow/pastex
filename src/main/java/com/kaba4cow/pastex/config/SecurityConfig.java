19a4a2fba04b348024bfd2296ebc9093b66e5ffe
kaba4cow <kaba4cow@gmail.com>
2025-08-01 21:20:01 +0300

    Add authentication

--- src/main/java/com/kaba4cow/pastex/config/SecurityConfig.java
+++ src/main/java/com/kaba4cow/pastex/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.kaba4cow.pastex.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.kaba4cow.pastex.auth.jwt.JwtAuthFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,12 +21,18 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 public class SecurityConfig {
 
+	private final JwtAuthFilter jwtAuthFilter;
+
+	private final AuthenticationProvider authProvider;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http//
 				.csrf(AbstractHttpConfigurer::disable)//
 				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))//
 				.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())//
+				.authenticationProvider(authProvider)//
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)//
 				.build();
 	}
 
