ac199786fcc6bb753a6e1052acae66ff27c64460
kaba4cow <kaba4cow@gmail.com>
2025-08-01 23:16:11 +0300

    Replace CurrentUser -> AuthenticationPrincipal

--- src/main/java/com/kaba4cow/pastex/domain/paste/controller/PasteController.java
+++ src/main/java/com/kaba4cow/pastex/domain/paste/controller/PasteController.java
@@ -3,6 +3,7 @@ package com.kaba4cow.pastex.domain.paste.controller;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.kaba4cow.pastex.auth.annotation.CurrentUser;
 import com.kaba4cow.pastex.domain.paste.dto.PasteCreateRequest;
 import com.kaba4cow.pastex.domain.paste.dto.PasteDto;
 import com.kaba4cow.pastex.domain.user.model.User;
@@ -21,14 +21,14 @@ public interface PasteController {
 	@PostMapping
 	ResponseEntity<PasteDto> createPaste(//
 			@RequestBody PasteCreateRequest request, //
-			@CurrentUser User author//
+			@AuthenticationPrincipal User author//
 	);
 
 	@GetMapping("/{id}")
 	ResponseEntity<PasteDto> getPaste(//
 			@PathVariable UUID id, //
 			@RequestParam(required = false) String password, //
-			@CurrentUser User requester//
+			@AuthenticationPrincipal User requester//
 	);
 
 }
