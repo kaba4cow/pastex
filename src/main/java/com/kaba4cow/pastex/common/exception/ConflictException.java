67b07b36571377d255e6f0174b4885edfc367002
kaba4cow <kaba4cow@gmail.com>
2025-08-01 21:14:32 +0300

    Create ConflictException

---
+++ src/main/java/com/kaba4cow/pastex/common/exception/ConflictException.java
@@ -0,0 +1,11 @@
+package com.kaba4cow.pastex.common.exception;
+
+public class ConflictException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ConflictException(String message) {
+		super(message);
+	}
+
+}
