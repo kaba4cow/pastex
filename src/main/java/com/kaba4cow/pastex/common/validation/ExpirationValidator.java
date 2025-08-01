27e3ffbe385941d0b90547162de8269469ed0272
kaba4cow <kaba4cow@gmail.com>
2025-08-01 11:23:55 +0300

    Fix ExpirationValidator

--- src/main/java/com/kaba4cow/pastex/common/validation/ExpirationValidator.java
+++ src/main/java/com/kaba4cow/pastex/common/validation/ExpirationValidator.java
@@ -1,11 +1,9 @@
 package com.kaba4cow.pastex.common.validation;
 
-import java.util.Objects;
-import java.util.regex.Matcher;
-
 import org.springframework.stereotype.Component;
 
-import com.kaba4cow.pastex.domain.paste.policy.ExpirationPolicy;
+import com.kaba4cow.pastex.domain.paste.service.ExpirationParseException;
+import com.kaba4cow.pastex.domain.paste.service.ExpirationService;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
@@ -15,41 +13,16 @@ import lombok.RequiredArgsConstructor;
 @Component
 public class ExpirationValidator implements ConstraintValidator<ValidExpiration, String> {
 
-	private final ExpirationPolicy expirationPolicy;
+	private final ExpirationService expirationService;
 
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
-		if (isBlank(value))
-			return true;
-		Matcher matcher = getMatcher(value);
-		if (!matcher.matches())
-			return false;
-		String unit = extractUnit(matcher);
-		if (!expirationPolicy.supportsUnit(unit))
-			return false;
 		try {
-			long amount = extractAmount(matcher);
-			long limit = expirationPolicy.getLimit(unit);
-			return amount > 0L && amount <= limit;
-		} catch (NumberFormatException exception) {
+			expirationService.parseExpiration(value);
+			return true;
+		} catch (ExpirationParseException exception) {
 			return false;
 		}
-	}
-
-	private boolean isBlank(String value) {
-		return Objects.isNull(value) || value.isBlank();
-	}
-
-	private Matcher getMatcher(String value) {
-		return expirationPolicy.getPattern().matcher(value);
-	}
-
-	private long extractAmount(Matcher matcher) {
-		return Long.parseLong(matcher.group(1));
-	}
-
-	private String extractUnit(Matcher matcher) {
-		return matcher.group(2);
 	}
 
 }
