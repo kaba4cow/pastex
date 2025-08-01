b3bb3ea8f5abaef1a6adfbd5c7583fa290d5b57a
kaba4cow <kaba4cow@gmail.com>
2025-08-01 23:37:09 +0300

    Add docs

--- src/main/java/com/kaba4cow/pastex/domain/paste/controller/PasteController.java
+++ src/main/java/com/kaba4cow/pastex/domain/paste/controller/PasteController.java
@@ -15,15 +15,36 @@ import com.kaba4cow.pastex.domain.paste.dto.PasteCreateRequest;
 import com.kaba4cow.pastex.domain.paste.dto.PasteDto;
 import com.kaba4cow.pastex.domain.user.model.User;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(//
+		name = "Pastes", //
+		description = """
+				Endpoints for paste creation and retrieval.
+				"""//
+)
 @RequestMapping("/api/pastes")
 public interface PasteController {
 
+	@Operation(//
+			summary = "Create a new paste", //
+			description = """
+					Creates a new paste with optional password and returns the paste data.
+					"""//
+	)
 	@PostMapping
 	ResponseEntity<PasteDto> createPaste(//
 			@RequestBody PasteCreateRequest request, //
 			@AuthenticationPrincipal User author//
 	);
 
+	@Operation(//
+			summary = "Get post by ID", //
+			description = """
+					Returns post by ID using password if paste is secured.
+					"""//
+	)
 	@GetMapping("/{id}")
 	ResponseEntity<PasteDto> getPaste(//
 			@PathVariable UUID id, //
