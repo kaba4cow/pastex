3d3f4659739556311776bbcb74d7310b76d9231c
kaba4cow <kaba4cow@gmail.com>
2025-08-02 11:32:31 +0300

    Remove PasteExpirationService

--- src/main/java/com/kaba4cow/pastex/domain/paste/service/PasteExpirationService.java
+++
@@ -1,9 +0,0 @@
-package com.kaba4cow.pastex.domain.paste.service;
-
-import java.time.LocalDateTime;
-
-public interface PasteExpirationService {
-
-	LocalDateTime computeExpiresAt(Long expiration);
-
-}
